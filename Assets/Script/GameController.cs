a137e850d2efea87059f4286a59e40489680a785
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-09-04 14:50:26 +0900

    ゲームコントローラの追加

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    static GameController _controller;
+
+    /// <summary>
+    /// ゲームコントローラのインスタンスを取得する．
+    /// </summary>
+    public static GameController Instance {
+        get
+        {
+            if (_controller == null)
+            {
+                var i = FindObjectOfType<GameController>();
+                if (i != null)
+                {
+                    _controller = i;
+                    return i;
+                }
+
+                var obj = new GameObject("Game Controller");
+                _controller = obj.AddComponent<GameController>();
+                DontDestroyOnLoad(obj);
+            }
+
+            return _controller;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
