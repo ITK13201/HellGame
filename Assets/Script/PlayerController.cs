b8c24d82fd1e82e54176cc0c5194da4d52c32da9
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-09-04 14:50:26 +0900

    ゲームコントローラの追加

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+    }
+}
