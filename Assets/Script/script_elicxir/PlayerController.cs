53e0c70e4703f3eee861c60be1d8a6c1ece66263
ELICXIR <elicxir536329@outlook.jp>
2020-09-05 23:13:38 +0900

    script_elicxirに移行。開始のカウントとか追加

---
+++ Assets/Script/script_elicxir/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb = null;
+
+    public Sprite sp1;//バ美肉の画像
+    public Sprite sp2;//普通の画像
+
+
+    public bool babiniku = true;//バ美肉状態かどうかのフラグ
+
+
+
+
+    public GameObject grid = null;
+
+    SpriteRenderer s = null;
+    Transform t = null;
+
+
+    private float rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rotate = grid.transform.rotation.eulerAngles.z;
+        s = GetComponent<SpriteRenderer>();
+        t = GetComponent<Transform>();
+    }
+
+    bool right = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        s.sortingOrder = 100 - (int)(t.position.y * 10);
+
+        if (babiniku)
+        {
+            s.sprite = sp1;
+
+        }
+        else s.sprite = sp2;
+
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        float vx;
+        float vy;
+
+
+        float speed =4.0f;
+        
+            vy = y*Mathf.Cos(rotate*3.1415f/180.0f) + x*Mathf.Sin(rotate * 3.1415f / 180.0f);
+            vx = -y*Mathf.Sin(rotate * 3.1415f / 180.0f) +x*Mathf.Cos(rotate * 3.1415f / 180.0f);
+
+            rb.velocity = new Vector2(vx*speed,vy*speed);
+
+
+        if (right && vx < -0.1f)
+        {
+            s.flipX = false;
+            right = false;
+        }
+        if (!right && vx > 0.1f)
+        {
+            s.flipX = true;
+            right = true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+       
+    }
+
+
+
+
+}
---
+++ Assets/Script/script_elicxir/layer_m.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layer_m : MonoBehaviour
+{
+    SpriteRenderer s = null;
+    Transform t = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<SpriteRenderer>();
+        t= GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        s.sortingOrder = 100 - (int)(t.position.y*10);
+    }
+}
