40cd23707b5c3c921957ad0654fe0aeddfbeab24
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-09-05 13:52:33 +0900

    配信視聴ステートを追加

--- Assets/Script/State/Player/PlayerState.cs
+++ Assets/Script/State/Player/PlayerState.cs
@@ -8,12 +8,28 @@ namespace HellGame.State
     /// </summary>
     public abstract class PlayerState : State<PlayerModel, PlayerStateType>
     {
-        public virtual void OnCollide(object target) {}
+        public virtual void OnCollide(object target) { }
+
+        public virtual void OnWatchTriggered()
+        {
+            if (Type == PlayerStateType.Watching)
+            {
+                return;
+            }
+
+            // TODO: 配信中かチェック
+
+            // 配信視聴モードに移行
+            StateMachine.NotifyNextState(new PlayerWatchingState(this));
+        }
+
+        public virtual void OnLeaveTriggered() { }
     }
 
     public enum PlayerStateType
     {
         Normal,
         Babiniku,
+        Watching,
     }
 }
---
+++ Assets/Script/State/Player/PlayerWatchingState.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using HellGame.Model;
+
+namespace HellGame.State
+{
+    public class PlayerWatchingState : PlayerState
+    {
+        public override PlayerStateType Type => PlayerStateType.Watching;
+        private PlayerState _previousState = null;
+
+        public PlayerWatchingState(PlayerState previous)
+        {
+            _previousState = previous;
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public void BackToPreviousState()
+        {
+            if (_previousState == null)
+            {
+                throw new NullReferenceException("the previous state is null, which is unexpected");
+            }
+
+            StateMachine.NotifyNextState(_previousState);
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("プレイヤー：配信視聴　へ変更します");
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("プレイヤー：配信視聴　から抜けます");
+        }
+
+        public override void OnLeaveTriggered()
+        {
+            BackToPreviousState();
+        }
+    }
+}
