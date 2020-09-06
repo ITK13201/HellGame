dd6da1e7e35f0f0495240cd9311179d9cc380702
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-09-06 09:52:52 +0900

    通知を表示できるように

--- Assets/Script/script_gamescene/Twitter.cs
+++ Assets/Script/script_gamescene/Twitter.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using HellGame;
+using HellGame.State;
+using HellGame.Model;
+
 public class Twitter : MonoBehaviour
 {
     int mes_num;
     public GameObject[] window = new GameObject[3];
     Twitter2[] sc = new Twitter2[3];
+    GameController m_gc;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int q = 0; q < 3; q++)
+        for (int q = 0; q < 3; q++)
         {
             sc[q] = window[q].GetComponent<Twitter2>();
         }
+
+        m_gc = GameController.EnsureGame;
+        m_gc.Model.Bias.StateMachine.StateMachineTransition += OnBiasStateChanged;
+    }
+
+    void OnDestroy()
+    {
+        m_gc = null;
     }
 
 
-    public void New_Message(string name ,string mes)
+    public void New_Message(string name, string mes)
     {
         for (int q = 0; q < 3; q++)
         {
@@ -33,5 +46,26 @@ public class Twitter : MonoBehaviour
         }
     }
 
+    void OnBiasStateChanged(IStateMachine<BiasModel, BiasStateType> sender_, BiasStateType type)
+    {
+        var sender = sender_ as StateMachine<BiasModel, BiasState, BiasStateType>;
 
+        // 配信中の場合
+        switch (sender.State.Type)
+        {
+            case BiasStateType.Streaming:
+                New_Message("Hogetter", $"白いメスケモが配信を開始しました");
+                break;
+            case BiasStateType.Inactive:
+                New_Message("Hogetter", $"白いメスケモが配信を終了しました");
+                break;
+            case BiasStateType.Preparing:
+                var s = sender.State as BiasPreparingState;
+                New_Message("Hogetter", $"白いメスケモがあと{s.TimeToReady}秒で配信を開始します");
+                break;
+            default:
+                // unreachable!()
+                break;
+        }
+    }
 }
